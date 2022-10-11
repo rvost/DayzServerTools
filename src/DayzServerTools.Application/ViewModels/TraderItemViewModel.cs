2e013a46661310f04150be62d5a4f037dd3d6dde
rvost <romanvost@gmail.com>
2022-10-11 15:25:54 +0300

    Add validation for TraderConfig
    * Add validation for TraderItem properties;
    * Add tooltips with error messages.

--- src/DayzServerTools.Application/ViewModels/TraderItemViewModel.cs
+++ src/DayzServerTools.Application/ViewModels/TraderItemViewModel.cs
@@ -2,34 +2,38 @@
 using CommunityToolkit.Mvvm.Input;
 
 using DayzServerTools.Library.Trader;
+using System.ComponentModel.DataAnnotations;
 
 namespace DayzServerTools.Application.ViewModels;
 
-public class TraderItemViewModel : ObservableObject
+public class TraderItemViewModel : ObservableValidator
 {
     private readonly TraderItem _model;
 
     public TraderItem Model => _model;
-
+    [MinLength(1)]
     public string Name
     {
         get => _model.Name;
-        set => SetProperty(_model.Name, value, _model, (m, v) => m.Name = v);
+        set => SetProperty(_model.Name, value, _model, (m, v) => m.Name = v, true);
     }
+    [RegularExpression(@"^(\*|W|M|V|VNK|K|S|[1-9]\d*)$", ErrorMessage ="Invalid Quantity Modifier")]
     public string Modifier
     {
         get => _model.Modifier;
-        set => SetProperty(_model.Modifier, value, _model, (m, v) => m.Modifier = v);
+        set => SetProperty(_model.Modifier, value, _model, (m, v) => m.Modifier = v, true);
     }
+    [Range(-1, double.MaxValue)]
     public double BuyPrice
     {
         get => _model.BuyPrice;
-        set => SetProperty(_model.BuyPrice, value, _model, (m, v) => m.BuyPrice = v);
+        set => SetProperty(_model.BuyPrice, value, _model, (m, v) => m.BuyPrice = v, true);
     }
+    [Range(-1, double.MaxValue)]
     public double SellPrice
     {
         get => _model.SellPrice;
-        set => SetProperty(_model.SellPrice, value, _model, (m, v) => m.SellPrice = v);
+        set => SetProperty(_model.SellPrice, value, _model, (m, v) => m.SellPrice = v, true);
     }
 
     public IRelayCommand ProhibitBuyingCommand { get; }
@@ -43,6 +47,7 @@ public class TraderItemViewModel : ObservableObject
         ProhibitSellingCommand = new RelayCommand(ProhibitSelling);
     }
 
+    public void ValidateSelf() => ValidateAllProperties();
     protected void ProhibitBuying()
     {
         BuyPrice = -1;
