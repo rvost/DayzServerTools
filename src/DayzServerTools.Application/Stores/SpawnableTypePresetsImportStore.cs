e4bcad6699469d889f39b79256790d4cbfb8bb9e
rvost <romanvost@gmail.com>
2022-12-01 23:55:17 +0300

    Clean SpawnableTypePresetsImportStore
    Remove unused code.

--- src/DayzServerTools.Application/Stores/SpawnableTypePresetsImportStore.cs
+++ src/DayzServerTools.Application/Stores/SpawnableTypePresetsImportStore.cs
@@ -1,12 +1,8 @@
 ﻿using System.Collections.ObjectModel;
 
-using CommunityToolkit.Mvvm.DependencyInjection;
-
 using DayzServerTools.Application.Extensions;
 using DayzServerTools.Application.ViewModels.SpawnableTypes;
-using DayzServerTools.Library.Common;
 using DayzServerTools.Library.Xml;
-using DayzServerTools.Library.Xml.Validators;
 
 namespace DayzServerTools.Application.Stores;
 
@@ -16,7 +12,7 @@ public class SpawnableTypePresetsImportStore : IClassnameImportStore
     private readonly PresetType _type;
     private readonly SpawnableTypesViewModelsFactory _viewModelsFactory;
 
-    public SpawnableTypePresetsImportStore(PresetType type, ObservableCollection<SpawnablePresetViewModel> target, 
+    public SpawnableTypePresetsImportStore(PresetType type, ObservableCollection<SpawnablePresetViewModel> target,
         SpawnableTypesViewModelsFactory viewModelsFactory)
     {
         _target = target;
@@ -31,13 +27,6 @@ public class SpawnableTypePresetsImportStore : IClassnameImportStore
             {
                 var preset = new SpawnablePreset() { Chance = 1 };
                 preset.Items.Add(item);
-                var provider = Ioc.Default.GetService<IRandomPresetsProvider>();
-                SpawnablePresetValidator validator = _type switch
-                {
-                    PresetType.Attachments => new SpawnablePresetValidator(() => provider.AvailableAttachmentsPresets),
-                    PresetType.Cargo => new SpawnablePresetValidator(() => provider.AvailableCargoPresets),
-                    _ => null
-                };
                 return _viewModelsFactory.CreateSpawnablePresetViewModel(_type, preset);
             });
         _target.AddRange(presetVMs);
