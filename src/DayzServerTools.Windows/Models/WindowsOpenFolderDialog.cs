aaed9027354bb62b987f87e486add418354c340f
rvost <romanvost@gmail.com>
2023-12-21 22:48:19 +0300

    refactor: Update WindowsOpenFolderDialog
    - Replace CommonOpenFileDialog with the built-in OpenFolderDialog;
    - Remove dependency on WindowsAPICodePack-Shell.

--- src/DayzServerTools.Windows/Models/WindowsOpenFolderDialog.cs
+++ src/DayzServerTools.Windows/Models/WindowsOpenFolderDialog.cs
@@ -1,12 +1,12 @@
 ﻿using DayzServerTools.Application.Models;
-using Microsoft.WindowsAPICodePack.Dialogs;
+using Microsoft.Win32;
 
 namespace DayzServerTools.Windows.Models;
 
 internal class WindowsOpenFolderDialog : IFolderDialog
 {
-    private readonly CommonOpenFileDialog _dialog;
-    public string FileName => _dialog.FileName;
+    private readonly OpenFolderDialog _dialog;
+    public string FileName => _dialog.FolderName;
     public string Title { get =>_dialog.Title; set { _dialog.Title = value; } }
 
     public bool? ShowDialog()
@@ -17,6 +17,6 @@ internal class WindowsOpenFolderDialog : IFolderDialog
 
     public WindowsOpenFolderDialog()
     {
-        _dialog= new CommonOpenFileDialog() { IsFolderPicker=true};
+        _dialog= new OpenFolderDialog();
     }
 }
